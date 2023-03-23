87e0eaca524f70ca4719ee2ec9faa867ec0e41b9
Josh <jnievelstein@yahoo.com>
2023-03-23 12:34:32 +0100

    Initial commit

---
+++ src/main/java/GeoData.java
@@ -0,0 +1,6 @@
+public class GeoData {
+
+    public GeoData() {
+
+    }
+}
---
+++ src/main/java/IFC.java
@@ -0,0 +1,11 @@
+public class IFC {
+    double x;
+    double y;
+    double z;
+
+    public IFC(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
